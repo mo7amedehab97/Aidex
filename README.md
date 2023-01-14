1cc3afad9747c449aeaa5d2b1c46db12d91b4d9d
mo7amedehab97 <gazacityface@gmail.com>
2023-01-14 19:11:48 +0200

    inital setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# Aidex
-Travel around japan - it's a new company provides a tours around japan
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+
+function App() {
+
+  return <div>hello workd</div>;
+}
+
+export default App;
